b189184d27744126d5172c632037fec1c0046c65
ujjothula <155039531+ujjothula@users.noreply.github.com>
2024-01-04 18:26:03 +0530

    Update models.py
    third commit

--- MyApps1/models.py
+++ MyApps1/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    sno = models.IntegerField()
-    sname = models.CharField(max_length=30)
-    height = models.FloatField()
+

f850e28e4e7cf7a23a12c53bfe767d563615b38e
ujjothula <155039531+ujjothula@users.noreply.github.com>
2024-01-04 18:27:06 +0530

    Update views.py
    third commit

--- MyApps1/views.py
+++ MyApps1/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
-
+def f1(request):
+    return HttpResponse("hello world")
+def f2(request):
+    return HttpResponse("welcome")
+    def f3(request):
+    return HttpResponse("python")
+    def f4(request):
+    return HttpResponse("python")
+     def f5(request):
+    return HttpResponse("ramesh")
